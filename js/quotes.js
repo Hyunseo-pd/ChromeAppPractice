b5d1712fe593062e7644d5403508a499716ea396
Hyunseo-pd <hseo100@naver.com>
2024-11-20 10:34:55 +0900

    classFinished

---
+++ js/quotes.js
@@ -0,0 +1,49 @@
+const quotes = [
+    {
+        quote: "Be yourself; everyone else is already taken.",
+        author: "Oscar Wilde"
+    },
+    {
+        quote: "Live as if you were to die tomorrow. Learn as if you were to live forever.",
+        author: "Mahatma Gandhi"
+    },
+    {
+        quote: "When one door of happiness closes, another opens; but often we look so long at the closed door that we do not see the one which has been opened for us.",
+        author: "Helen Keller"
+    },
+    {
+        quote: "Not all those who wander are lost.",
+        author: "J.R.R. Tolkien, The Fellowship of the Ring"
+    },
+    {
+        quote: "Well done is better than well said.",
+        author: "Benjamin Franklin"
+    },
+    {
+        quote: "It is during our darkest moments that we must focus to see the light.",
+        author: "Aristotle"
+    },
+    {
+        quote: "Do not go where the path may lead, go instead where there is no path and leave a trail",
+        author: "Ralph Waldo Emerson"
+    },
+    {
+        quote: "Go confidently in the direction of your dreams! Live the life you've imagined",
+        author: "Henry David Thoreau"
+    },
+    {
+        quote: "Life is trying things to see if they work.",
+        author: "Ray Bradbury"
+    },
+    {
+        quote: "Success usually comes to those who are too busy to be looking for it.",
+        author: "Henry David Thoreau"
+    }
+]
+
+const quote = document.querySelector("#quote span:first-child")
+const author = document.querySelector("#quote span:last-child")
+
+const todaysQuote = quotes[Math.floor(Math.random()*quotes.length)];
+quote.innerText = todaysQuote.quote;
+author.innerText = todaysQuote.author;
---
+++ js/todo.js
@@ -0,0 +1,53 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("input")
+const toDoList = document.getElementById("todo-list");
+const TODOS_KEY = "todos"
+
+let toDos = [];
+
+function deleteToDo(event) {
+    const li = event.target.parentElement;
+    toDos = toDos.filter((todos) => todos.id !== parseInt(li.id));
+    li.remove();
+    saveToDos();
+}
+
+function saveToDos() {
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
+function paintToDo(newTodoObj) {
+    const li = document.createElement("li");
+    li.id = newTodoObj.id;
+    const span = document.createElement("span");
+    span.innerText = newTodoObj.text;
+    const button = document.createElement("button");
+    button.innerText = "X";
+    button.addEventListener("click", deleteToDo);
+    li.appendChild(span);
+    li.appendChild(button);
+    toDoList.append(li);
+}
+
+function handleToDoSubmit(event) {
+    event.preventDefault();
+    const newTodo = toDoInput.value;
+    toDoInput.value = "";
+    const newTodoObj = {
+        text: newTodo,
+        id: Date.now(),
+    }
+    toDos.push(newTodoObj);
+    paintToDo(newTodoObj);
+    saveToDos();
+}
+
+toDoForm.addEventListener("submit",handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if(savedToDos !== null) {
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo);
+}
